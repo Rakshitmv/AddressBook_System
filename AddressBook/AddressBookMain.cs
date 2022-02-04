1fd16e83c59baf9e67e302a5135d0fd821feb600
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-04 23:14:33 +0530

    [Rakshit] Add: UC5_Add multiple contact in Address book program completed

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -11,27 +11,33 @@ namespace AddressBook
         List<Contacts> addressBook = new List<Contacts>();
         public void AddContact()
         {
-            Contacts contacts = new Contacts();
-            Console.WriteLine("Enter First Name\n");
-            contacts.FirstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name\n");
-            contacts.LastName = Console.ReadLine();
-            Console.WriteLine("Enter Address\n");
-            contacts.Address = Console.ReadLine();
-            Console.WriteLine("Enter city\n");
-            contacts.City = Console.ReadLine();
-            Console.WriteLine("Enter State\n");
-            contacts.State = Console.ReadLine();
-            Console.WriteLine("Enter Zip\n");
-            contacts.Zip = Console.ReadLine();
-            Console.WriteLine("Enter Phone Number\n");
-            contacts.PhoneNumber = Console.ReadLine();
-            Console.WriteLine("Enter Email");
-            contacts.Email = Console.ReadLine();
-            Console.WriteLine("Created Contacts:\n " + contacts.FirstName + "\n" + contacts.LastName + "\n" + contacts.Address + "\n" + contacts.City + "\n" + contacts.State + "\n" + contacts.Zip + "\n" + contacts.PhoneNumber + "\n" + contacts.Email + "\n");
+            Console.Write("Add multiple person details : ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            while (Num > 0)
+            {
+                Contacts contacts = new Contacts();
+                Console.WriteLine("Enter First Name\n");
+                contacts.FirstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name\n");
+                contacts.LastName = Console.ReadLine();
+                Console.WriteLine("Enter Address\n");
+                contacts.Address = Console.ReadLine();
+                Console.WriteLine("Enter city\n");
+                contacts.City = Console.ReadLine();
+                Console.WriteLine("Enter State\n");
+                contacts.State = Console.ReadLine();
+                Console.WriteLine("Enter Zip\n");
+                contacts.Zip = Console.ReadLine();
+                Console.WriteLine("Enter Phone Number\n");
+                contacts.PhoneNumber = Console.ReadLine();
+                Console.WriteLine("Enter Email");
+                contacts.Email = Console.ReadLine();
+                Console.WriteLine("Created Contacts:\n " + contacts.FirstName + "\n" + contacts.LastName + "\n" + contacts.Address + "\n" + contacts.City + "\n" + contacts.State + "\n" + contacts.Zip + "\n" + contacts.PhoneNumber + "\n" + contacts.Email + "\n");
 
-            addressBook.Add(contacts);
-            Console.WriteLine("{0}Contact has successfully added", contacts.FirstName);
+                addressBook.Add(contacts);
+                Console.WriteLine("{0}Contact has successfully added", contacts.FirstName);
+                Num--;
+            }
         }
         public void EditContact(string firstName)
         {
@@ -94,6 +100,10 @@ namespace AddressBook
                     }
 
                 }
+                else
+                {
+                    Console.WriteLine("Contact of the person {0} does not exist : ", firstName);
+                }
             }
         }
 
@@ -109,6 +119,7 @@ namespace AddressBook
                     Console.WriteLine("{0} Contact Successfully Deleted", contact.FirstName);
                     return;
                 }
+
             }
         }
 
